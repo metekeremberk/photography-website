482ab2b2794334ad72461316dfd74bb2bce6f272
metekeremberk <metekeremberk@gmail.com>
2023-04-08 00:56:21 +0300

    Landing page added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { Lexend } from "next/font/google";
+import type { AppProps } from "next/app";
+
+const lexend = Lexend({ weight: ["400", "500", "600", "700"], subsets: ["latin"], variable: "--font-lexend" });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<main className={`${lexend.variable} font-sans`}>
+			<Component {...pageProps} />
+		</main>
+	);
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,23 @@
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-    './src/app/**/*.{js,ts,jsx,tsx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
+	content: ["./src/pages/**/*.{js,ts,jsx,tsx}", "./src/components/**/*.{js,ts,jsx,tsx}", "./src/app/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {
+			colors: {
+				palettePrimary: "#252323",
+				paletteSecondary: "#8A817C",
+				paletteTertiary: "#BCB8B1",
+				paletteQuaternary: "#F4F3EE",
+				paletteRed: "#B3001B",
+				paletteGreen: "#0B6E4F",
+				paletteBlue: "#2541B2",
+			},
+			fontFamily: {
+				sans: ["var(--font-lexend)", ...fontFamily.sans],
+			},
+		},
+	},
+	plugins: [],
+};
